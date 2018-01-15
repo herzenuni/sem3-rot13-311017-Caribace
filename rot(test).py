c40e853e71c8ffd591bd65329caa2e26c1e3e547
Mikhail Nikanorov <31664472+Caribace@users.noreply.github.com>
2018-01-15 21:47:57 +0300

    Create rot(test).py

---
+++ rot(test).py
@@ -0,0 +1,12 @@
+"""
+Этот модуль тестирует функцию шифрования алгоритмом rot13
+"""
+def test_summ(count,expected):
+	assert(rot13(count) == expected)
+import rot13 as r
+def test_Rot13(s):
+  """
+  Функция проверяет функцию шифрования Rot13
+  """
+  assert(r.rot13(r.rot13(s)) == s) 
+  return r.rot13(r.rot13(s)) == s
